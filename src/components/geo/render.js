4dee6b52d7d58c64cdb3aa8a36d6eae9c62e286a
james hadfield <jh22@sanger.ac.uk>
2019-04-12 23:22:20 +0545

    add map legend, titles etc

--- src/components/geo/render.js
+++ src/components/geo/render.js
@@ -1,9 +1,9 @@
 import { select } from "d3-selection";
 import 'd3-transition';
 import { geoPath, geoMercator } from "d3-geo";
-import { scaleSequential } from "d3-scale";
+import { scaleLinear, scaleSequential } from "d3-scale";
 import { interpolateBlues } from "d3-scale-chromatic";
-
+import { axisBottom } from "d3-axis";
 
 /* TO DO -- we are simply using the selector designed for the table.
    we should build a better one to avoid all the data transforming here!
@@ -20,7 +20,7 @@ const transformToPercentages = (categories, flatData) => {
     const total = categories.map((c) => d[c]).reduce((acc, cv) => acc+cv, 0);
     demePercs[deme] = d[vizCategory] / total * 100;
   });
-  return demePercs;
+  return [vizCategory, demePercs];
 };
 
 
@@ -37,21 +37,46 @@ const transformToPercentages = (categories, flatData) => {
  */
 export const renderMap = ({ref, width, height, resultsData, geoJsonData, geoResolution, geoLinks}) => {
 
-  if (!resultsData || !geoJsonData || !ref) {
-    return undefined;
-  }
+  if (!resultsData || !geoJsonData || !ref) return undefined;
 
   const {primaryVariable, flatData, categories, groupByVariable, groupByValue} = resultsData;
-  const titleText = `Map of ${geoResolution.label} showing ${primaryVariable.label}${groupByVariable ? ` with ${groupByVariable.label} restricted to ${groupByValue}` : ""}`;
 
+  let mainTitle = primaryVariable.label;
+  const secondaryTitle = groupByVariable ? `${groupByVariable.label} restricted to ${groupByValue}` : "";
+
+  const dims = {
+    x1: 0, /* left margin, measured L-R */
+    x2: width, /* right margin, measured L-R */
+    y1: 90, /* top margin, measured T-B */
+    y2: height, /* bottom margin, measured T-B */
+    /* LEGEND */
+    legx1: 10,
+    legx2: width-10,
+    legy1: 60,
+    legHeight: 10,
+    /* TITLES */
+    tity1: 20,
+    tity2: 40
+  };
+
+  /* D3 path generator for demes -- see https://github.com/d3/d3-geo */
+  const geoPathGenerator = geoPath()
+    .projection(
+      geoMercator()
+        .fitExtent([[dims.x1, dims.y1], [dims.x2-dims.x1, dims.y2-dims.y1]], geoJsonData)
+    );
+
+  /* calculate fills, legends etc based on the data type */
   let fill; // will be passed to d3, so can be a string or a function receiving a data point
+  let legend;
   if (categories.length !== 2) {
     console.warn(`Cannot display simple chloropleth for this ${primaryVariable.label} with ${categories.length} categories`);
     fill = unknownFill;
+    mainTitle += " (viz not implemented)";
   } else {
-    const demePercs = transformToPercentages(categories, flatData);
+    const [vizCategory, demePercs] = transformToPercentages(categories, flatData);
     const maxPerc = Object.keys(demePercs).reduce((res, deme) => demePercs[deme] > res ? demePercs[deme] : res, 0);
-
+    mainTitle += `. Showing % ${vizCategory}`;
     /* Fn to extract the deme name from an individual geoJSON feature */
     const getDemeName = (d) => geoLinks[d.properties.GEOID][geoResolution.value];
 
@@ -62,32 +87,39 @@ export const renderMap = ({ref, width, height, resultsData, geoJsonData, geoReso
       const deme = getDemeName(d);
       return colourScale(demePercs[deme]);
     };
+
+    legend = {};
+    legend.scale = colourScale;
+    legend.axis = axisBottom()
+      .scale(
+        scaleLinear()
+          .range([dims.legx1, dims.legx2-dims.legx1])
+          .domain(colourScale.domain())
+      )
+      .tickSize(6)
+      .ticks(8)
+      .tickFormat((d) => `${d}%`);
   }
 
-
-  /* D3 path generator for demes -- see https://github.com/d3/d3-geo */
-  const geoPathGenerator = geoPath()
-    .projection(
-      geoMercator()
-        .fitExtent([[0, 0], [width, height]], geoJsonData)
-    );
-
-
+  /* basic SVG set up */
   select(ref)
     .selectAll("*")
     .remove();
-
-  /* mainly for debugging -- to be improved! */
-  select(ref)
-    .append("span")
-    .text(titleText);
-
   const svg = select(ref)
     .append('svg')
     .style('background-color', 'white')
     .attr('width', width)
     .attr('height', height);
 
+  /* render the (text) titles */
+  svg.append("text")
+    .attr("transform", `translate(${dims.legx1}, ${dims.tity1})`)
+    .text(mainTitle);
+  svg.append("text")
+    .attr("transform", `translate(${dims.legx1}, ${dims.tity2})`)
+    .text(secondaryTitle);
+
+  /* render the actual geo shapes, apply fills etc */
   svg.append("g")
     .attr("class", "geoShapes")
     .selectAll("path")
@@ -98,5 +130,40 @@ export const renderMap = ({ref, width, height, resultsData, geoJsonData, geoReso
     .attr("stroke", "white")
     .attr("d", geoPathGenerator);
 
+
+  /* L E G E N D */
+  if (legend) {
+    /* the axis (the simple bit) */
+    svg.append("g")
+      .attr("class", "legend axis")
+      .attr("transform", `translate(0, ${dims.legy1})`)
+      .call(legend.axis);
+
+    /* the gradient (the harder bit) */
+    /* https://www.visualcinnamon.com/2016/05/smooth-color-legend-d3-svg-gradient.html */
+    const linearGradient = svg.append("defs")
+      .append("linearGradient")
+      .attr("id", "linear-gradient");
+
+    linearGradient.selectAll("stop")
+      .data([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]) /* gradient percs */
+      .enter()
+      .append("stop")
+      .attr("offset", (d) => `${d}%`)
+      .attr("stop-color", (d) => {
+        const chloroPerc = (d/100) * (legend.scale.domain()[1] - legend.scale.domain()[0]) + legend.scale.domain()[0];
+        return legend.scale(chloroPerc);
+      });
+
+    svg.append("g")
+      .attr("class", "legend gradient")
+      .append("rect")
+      .attr("width", dims.legx2-dims.legx1)
+      .attr("height", dims.legHeight)
+      .attr("transform", `translate(${dims.legx1}, ${dims.legy1-dims.legHeight+1})`)
+      .style("fill", "url(#linear-gradient)");
+  }
+
   return undefined;
 };
+
